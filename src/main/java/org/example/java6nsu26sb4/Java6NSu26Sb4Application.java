1a60a98fe27fd53942c5301608d2464701729884
huy19712011 <35172019+huy19712011@users.noreply.github.com>
2026-06-25 21:18:45 +0700

    spring boot 4 project

---
+++ src/main/java/org/example/java6nsu26sb4/Java6NSu26Sb4Application.java
@@ -0,0 +1,15 @@
+package org.example.java6nsu26sb4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java6NSu26Sb4Application {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(Java6NSu26Sb4Application.class, args);
+        System.out.println("running...");
+    }
+
+}
---
+++ src/test/java/org/example/java6nsu26sb4/Java6NSu26Sb4ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.java6nsu26sb4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Java6NSu26Sb4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
