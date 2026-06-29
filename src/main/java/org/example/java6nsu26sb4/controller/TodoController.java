fc4c2e6fc5aec1505d90448138fb26306e7d34c2
huy19712011 <35172019+huy19712011@users.noreply.github.com>
2026-06-29 20:15:21 +0700

    API basics: GET, POST, PUT, DELETE

---
+++ src/main/java/org/example/java6nsu26sb4/controller/TodoController.java
@@ -0,0 +1,51 @@
+package org.example.java6nsu26sb4.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.example.java6nsu26sb4.entity.Todo;
+import org.example.java6nsu26sb4.service.TodoService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/todos")
+public class TodoController {
+
+    private final TodoService todoService;
+
+    // api/todos
+    @GetMapping
+    public List<Todo> getAllTodos() {
+
+        return todoService.findAll();
+    }
+
+    // api/todos/id
+    @GetMapping("{id}")
+    public Todo getTodo(@PathVariable long id) {
+
+        return todoService.findById(id);
+    }
+
+    // api/todos
+    @PostMapping
+    public Todo addTodo(@RequestBody Todo todo) {
+
+        return todoService.add(todo);
+    }
+
+    // api/todos/id
+    @PutMapping("{id}")
+    public Todo updateTodo(@RequestBody Todo todo, @PathVariable long id) {
+
+        return todoService.update(todo, id);
+    }
+
+    // api/todos/id
+    @DeleteMapping("{id}")
+    public Todo deleteTodo(@PathVariable long id) {
+
+        return todoService.delete(id);
+    }
+}
