fff5d5e4d307bad06cdc0ff0fcec06fb0f38428d
huy19712011 <35172019+huy19712011@users.noreply.github.com>
2026-07-11 21:54:17 +0700

    API basics: validation + global exception

--- src/main/java/org/example/java6nsu26sb4/entity/Todo.java
+++ src/main/java/org/example/java6nsu26sb4/entity/Todo.java
@@ -1,6 +1,7 @@
 package org.example.java6nsu26sb4.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -16,7 +17,12 @@ public class Todo {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Size(min = 3, max = 100, message = "Title must be between 3 and 100 characters")
     private String title;
+
+    @Size(min = 3, message = "Description should have at least 3 characters")
     private String description;
+
     private boolean completed;
 }
